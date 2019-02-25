99957c93f097db7d47b856874b13cdb7afa82b18
vslobo <antilamer1709@gmail.com>
2019-02-25 12:14:36 +0200

    first commit

---
+++ src/main/java/com/antilamer/thingTracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.antilamer.thingTracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThingTrackerApplication.class);
+	}
+
+}
---
+++ src/main/java/com/antilamer/thingTracker/ThingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.antilamer.thingTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingTrackerApplication.class, args);
+	}
+
+}
