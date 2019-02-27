4e60de200a0def19292fe55b16b839d66c5a3942
vslobo <antilamer1709@gmail.com>
2019-02-27 18:42:42 +0200

    autocomplete component

--- front/src/app/main/expenses/add-expense/add-expense.component.ts
+++ front/src/app/main/expenses/add-expense/add-expense.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ExpensesService} from "../expenses.service";
 
 @Component({
   selector: 'app-add-expense',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddExpenseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private expensesService: ExpensesService) { }
 
   ngOnInit() {
+  }
+
+
+  texts: string[] = [];
+  results: string[];
+
+  search(event) {
+    this.results = ["aaa", "bbb", "ccc"];
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    if (event.key == "Enter") {
+      let tokenInput = event.srcElement as any;
+      if (tokenInput.value) {
+        this.texts.push(tokenInput.value);
+        tokenInput.value = "";
+      }
+    }
   }
 
 }
--- front/src/app/main/expenses/expenses.module.ts
+++ front/src/app/main/expenses/expenses.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddExpenseComponent } from './add-expense/add-expense.component';
+import {AutoCompleteModule} from "primeng/primeng";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [AddExpenseComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    AutoCompleteModule
   ]
 })
 export class ExpensesModule { }
