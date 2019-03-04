511396a5d01e25c57f8b604406a9866c681134e2
vslobo <antilamer1709@gmail.com>
2019-03-04 13:18:31 +0200

    changed Title in index

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -4,7 +4,6 @@ import {Router} from "@angular/router";
 import {AuthenticationService} from "./authentication/authentication.service";
 import {MessageService} from "primeng/components/common/messageservice";
 import {AppService} from "./app.service";
-import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -18,9 +17,7 @@ export class AppComponent implements OnInit {
   constructor(public authenticationService: AuthenticationService,
               public appService: AppService,
               private messageService: MessageService,
-              private titleService: Title,
               private router: Router) {
-    this.titleService.setTitle("Thing tracker");
   }
 
   ngOnInit(): void {
