7d4bc115645c4a762b652c8cc4d95a50a8580918
vslobo <antilamer1709@gmail.com>
2019-03-07 13:53:33 +0200

    registration, auto login

--- front/src/app/authentication/registration/registration.component.ts
+++ front/src/app/authentication/registration/registration.component.ts
@@ -3,8 +3,10 @@ import {RegistrationService} from "./registration.service";
 import {MessageService} from "primeng/components/common/messageservice";
 import {Router} from "@angular/router";
 import {FormGroup} from "@angular/forms";
-import {RegistrationDTO} from "../../../generated/dto";
+import {RegistrationDTO, UserDTO} from "../../../generated/dto";
 import {AppService} from "../../app.service";
+import {LoginService} from "../login/login.service";
+import {AuthenticationService} from "../authentication.service";
 
 @Component({
   selector: 'app-registration',
@@ -16,6 +18,8 @@ export class RegistrationComponent implements OnInit {
   public registration: RegistrationDTO;
 
   constructor(private service: RegistrationService,
+              private loginService: LoginService,
+              private authenticationService: AuthenticationService,
               private router: Router,
               private appService: AppService,
               private messageService: MessageService) {
@@ -31,9 +35,8 @@ export class RegistrationComponent implements OnInit {
       this.appService.blockedUI = true;
       this.service.register(this.registration).subscribe(
         () => {
-          this.appService.blockedUI = false;
-          this.messageService.add({severity:'success', summary:'Registration', detail:'You can log in now!'});
-          this.router.navigate(['/authentication/login']);
+          this.messageService.add({severity:'success', summary:'Registration', detail:'Registration has been successfully completed!'});
+          this.login();
         }
       );
     } else {
@@ -42,6 +45,23 @@ export class RegistrationComponent implements OnInit {
   }
 
   public login(): void {
+    let user: UserDTO = new UserDTO();
+    user.username = this.registration.username;
+    user.password = this.registration.password;
+
+    this.loginService.authenticate(user).subscribe(
+      (res) => {
+        console.log(res);
+        this.appService.blockedUI = false;
+
+        this.messageService.add({severity:'info', summary:'Hello', detail:'You are logged in!'});
+        this.authenticationService.loggedUser = res;
+        this.router.navigate(['/']);
+      }
+    );
+  }
+
+  public loginPage(): void {
     this.router.navigate(['login']);
   }
 
