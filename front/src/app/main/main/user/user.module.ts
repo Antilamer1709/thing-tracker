77ba4441f851c29ca8ffd8bc89482b53b39acda5
vslobo <antilamer1709@gmail.com>
2019-03-12 17:15:25 +0200

    add-to-group component

--- front/src/app/main/main/user/user.module.ts
+++ front/src/app/main/main/user/user.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyGroupComponent } from './my-group/my-group.component';
+import { AddToGroupComponent } from './add-to-group/add-to-group.component';
+import {AppRoutingModule} from "../../../app-routing.module";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
-  declarations: [MyGroupComponent],
+  declarations: [MyGroupComponent, AddToGroupComponent],
   imports: [
-    CommonModule
+    AppRoutingModule,
+    CommonModule,
+    ButtonModule
   ]
 })
 export class UserModule { }
--- front/src/app/main/main/user/user.routes.ts
+++ front/src/app/main/main/user/user.routes.ts
@@ -1,9 +1,13 @@
 import {Route} from "@angular/router";
 import {GuardService} from "../../../common/guard-service";
 import {MyGroupComponent} from "./my-group/my-group.component";
+import {AddToGroupComponent} from "./add-to-group/add-to-group.component";
 
 export const MyGroupRoutes: Route[] = [
   {
     path: 'my-group', component: MyGroupComponent, canActivate: [GuardService]
+  },
+  {
+    path: 'add-to-group', component: AddToGroupComponent, canActivate: [GuardService]
   }
 ];
