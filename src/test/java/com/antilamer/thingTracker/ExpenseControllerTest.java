eeea6786805f41d5a2f324308c2b37e211ef84da
vslobo <antilamer1709@gmail.com>
2019-09-17 17:08:29 +0300

    ExpenseControllerTest, ArgumentCaptor

--- src/test/java/com/antilamer/thingTracker/ExpenseControllerTest.java
+++ src/test/java/com/antilamer/thingTracker/ExpenseControllerTest.java
@@ -1,20 +1,17 @@
 package com.antilamer.thingTracker;
 
-import com.antilamer.thingTracker.config.WebSecurityConfig;
 import com.antilamer.thingTracker.controller.ExpenseController;
 import com.antilamer.thingTracker.dto.ExpenseDTO;
 import com.antilamer.thingTracker.service.ExpenseBO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +20,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.ArrayList;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,9 +45,8 @@ public class ExpenseControllerTest {
 
     @Test
     @WithMockUser(username = "user1", password = "user1", roles = "USER")
-    public void whenSaveValidExpense_thenReturns200() throws Exception {
+    public void whenUserSavesValidExpense_thenReturns200() throws Exception {
         ExpenseDTO expenseDTO = new ExpenseDTO();
-        expenseDTO.setId(1);
         expenseDTO.setPrice(500);
         expenseDTO.setTypes(new ArrayList<>());
         expenseDTO.getTypes().add("Food");
@@ -61,6 +59,12 @@ public class ExpenseControllerTest {
                 .content(objectMapper.writeValueAsString(expenseDTO)))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk());
+
+        ArgumentCaptor<ExpenseDTO> userCaptor = ArgumentCaptor.forClass(ExpenseDTO.class);
+        verify(expenseBO, times(1)).createExpense(userCaptor.capture());
+
+        assertEquals("Prices are not equals", 500, userCaptor.getValue().getPrice());
+        assertEquals("Types are not equals", "Food", userCaptor.getValue().getTypes().get(0));
     }
 
 }
