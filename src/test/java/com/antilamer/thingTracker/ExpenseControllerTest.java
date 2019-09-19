8e7137e5af1d508ef1f91dd62306f637de241ce8
vslobo <antilamer1709@gmail.com>
2019-09-19 15:08:07 +0300

    ExpenseControllerTest, userSearchChartEmpty_thenReturns200

--- src/test/java/com/antilamer/thingTracker/ExpenseControllerTest.java
+++ src/test/java/com/antilamer/thingTracker/ExpenseControllerTest.java
@@ -2,8 +2,11 @@ package com.antilamer.thingTracker;
 
 import com.antilamer.thingTracker.controller.ExpenseController;
 import com.antilamer.thingTracker.dto.ExpenseDTO;
+import com.antilamer.thingTracker.dto.ExpenseSearchChartDTO;
+import com.antilamer.thingTracker.dto.ExpenseSearchDTO;
 import com.antilamer.thingTracker.service.ExpenseBO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -15,11 +18,14 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.ArrayList;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -79,11 +85,35 @@ public class ExpenseControllerTest {
                 .param("predicate", "Foo"))
                 .andExpect(status().isOk());
 
-
         ArgumentCaptor<String> userCaptor = ArgumentCaptor.forClass(String.class);
         verify(expenseBO, times(1)).searchExpenseTypes(userCaptor.capture());
 
         assertEquals("Predicates are not equals", "Foo", userCaptor.getValue());
+    }
+
+
+    // searchChart
+    @Test
+    @WithMockUser(username = "user1", password = "user1", roles = "USER")
+    public void userSearchChartEmpty_thenReturns200() throws Exception {
+        ExpenseSearchChartDTO resultDTO = new ExpenseSearchChartDTO();
+        given(expenseBO.searchChart(any())).willReturn(resultDTO);
+        ExpenseSearchDTO expenseSearchDTO = new ExpenseSearchDTO();
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/expense/search/chart")
+                .with(csrf())
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(expenseSearchDTO)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String actualResponseBody = mvcResult.getResponse().getContentAsString();
+        assertThat(objectMapper.writeValueAsString(resultDTO)).isEqualToIgnoringWhitespace(actualResponseBody);
+
+        ArgumentCaptor<ExpenseSearchDTO> userCaptor = ArgumentCaptor.forClass(ExpenseSearchDTO.class);
+        verify(expenseBO, times(1)).searchChart(userCaptor.capture());
     }
 
 }
