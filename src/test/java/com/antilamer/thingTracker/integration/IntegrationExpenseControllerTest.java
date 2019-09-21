fb665ef9c486567fe6db78c945db113ebc0f2847
antilamer1709 <antilamer1709@gmail.com>
2019-09-21 18:42:51 +0300

    refactoring of integration tests

--- src/test/java/com/antilamer/thingTracker/integration/IntegrationExpenseControllerTest.java
+++ src/test/java/com/antilamer/thingTracker/integration/IntegrationExpenseControllerTest.java
@@ -18,7 +18,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
