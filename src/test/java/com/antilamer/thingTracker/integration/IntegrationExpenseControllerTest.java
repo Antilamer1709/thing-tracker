fb2579742abb100cba2e66ce2f4a728dc7b7c645
antilamer1709 <antilamer1709@gmail.com>
2019-09-21 11:14:48 +0300

    refactoring of tests

--- src/test/java/com/antilamer/thingTracker/integration/IntegrationExpenseControllerTest.java
+++ src/test/java/com/antilamer/thingTracker/integration/IntegrationExpenseControllerTest.java
@@ -1,67 +1,113 @@
 package com.antilamer.thingTracker.integration;
 
+import com.antilamer.thingTracker.Utils;
 import com.antilamer.thingTracker.config.TestConfig;
 import com.antilamer.thingTracker.controller.ExpenseController;
 import com.antilamer.thingTracker.dto.ExpenseDTO;
 import com.antilamer.thingTracker.dto.ExpenseSearchChartDTO;
 import com.antilamer.thingTracker.dto.ExpenseSearchDTO;
+import com.antilamer.thingTracker.model.UserEntity;
+import com.antilamer.thingTracker.repository.*;
+import com.antilamer.thingTracker.security.JwtTokenProvider;
+import com.antilamer.thingTracker.service.AuthenticationBOImpl;
 import com.antilamer.thingTracker.service.ExpenseBO;
+import com.antilamer.thingTracker.service.ExpenseBOImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static com.antilamer.thingTracker.Utils.doTestPost;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {ExpenseController.class, ObjectMapper.class, TestConfig.class})
-@EnableWebMvc
-@AutoConfigureMockMvc
+@DataJpaTest
+@Import(value = {ExpenseController.class, ObjectMapper.class, TestConfig.class, ExpenseBOImpl.class, AuthenticationBOImpl.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationExpenseControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Autowired
-    private ObjectMapper objectMapper;
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private ExpenseBO expenseBO;
+
+    @Autowired
+    private ExpenseRepo expenseRepo;
+
+    @Autowired
+    private ExpenseTypeDictRepo expenseTypeDictRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private GroupRepo groupRepo;
+
+    @Autowired
+    private RoleRepo roleRepo;
 
     @MockBean
-    private ExpenseBO expenseBO;
+    private PasswordEncoder passwordEncoder;
+
+    @MockBean
+    private AuthenticationManager authenticationManager;
+
+    @MockBean
+    private JwtTokenProvider tokenProvider;
+
+    @Before
+    public void onSetUpTestUser() {
+        UserEntity loggedUser = Utils.createDefaultUser();
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(loggedUser, null);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(new ExpenseController(expenseBO)).build();
+    }
 
 
     // createExpense
     @Test
-    @WithMockUser(username = "user1", password = "user1", roles = "USER")
     public void userSavesValidExpense_thenReturns200() throws Exception {
         ExpenseDTO expenseDTO = new ExpenseDTO();
         expenseDTO.setPrice(500);
         expenseDTO.getTypes().add("Food");
 
 
-        doTestPost(mockMvc, objectMapper, "/api/expense", expenseDTO);
-
-        ArgumentCaptor<ExpenseDTO> userCaptor = ArgumentCaptor.forClass(ExpenseDTO.class);
-        verify(expenseBO, times(1)).createExpense(userCaptor.capture());
-
-        assertEquals("Prices are not equals", 500, userCaptor.getValue().getPrice());
-        assertEquals("Types are not equals", "Food", userCaptor.getValue().getTypes().get(0));
+        mockMvc.perform(post("/api/expense")
+                .with(csrf())
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(expenseDTO)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk());
     }
 
 
@@ -74,11 +120,6 @@ public class IntegrationExpenseControllerTest {
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .param("predicate", "Foo"))
                 .andExpect(status().isOk());
-
-        ArgumentCaptor<String> userCaptor = ArgumentCaptor.forClass(String.class);
-        verify(expenseBO, times(1)).searchExpenseTypes(userCaptor.capture());
-
-        assertEquals("Predicates are not equals", "Foo", userCaptor.getValue());
     }
 
 
@@ -86,23 +127,15 @@ public class IntegrationExpenseControllerTest {
     @Test
     @WithMockUser(username = "user1", password = "user1", roles = "USER")
     public void userSearchChartPopulated_thenReturns200() throws Exception {
-        ExpenseSearchChartDTO mockResultDTO = new ExpenseSearchChartDTO();
-        mockResultDTO.getData().put("Food", 5000);
-        mockResultDTO.getData().put("Car", 15000);
-
-        given(expenseBO.searchChart(any())).willReturn(mockResultDTO);
+        ExpenseSearchChartDTO expectedResult = new ExpenseSearchChartDTO();
         ExpenseSearchDTO expenseSearchDTO = new ExpenseSearchDTO();
 
         MvcResult mvcResult = doTestPost(mockMvc, objectMapper, "/api/expense/search/chart", expenseSearchDTO);
 
         String actualResponseBody = mvcResult.getResponse().getContentAsString();
-        assertThat(objectMapper.writeValueAsString(mockResultDTO)).isEqualToIgnoringWhitespace(actualResponseBody);
 
         ExpenseSearchChartDTO actualResponseDTO = objectMapper.readValue(actualResponseBody, ExpenseSearchChartDTO.class);
-        assertThat(actualResponseDTO).isEqualTo(mockResultDTO);
-
-        ArgumentCaptor<ExpenseSearchDTO> userCaptor = ArgumentCaptor.forClass(ExpenseSearchDTO.class);
-        verify(expenseBO, times(1)).searchChart(userCaptor.capture());
+        assertThat(actualResponseDTO).isEqualTo(expectedResult);
     }
 
 }
