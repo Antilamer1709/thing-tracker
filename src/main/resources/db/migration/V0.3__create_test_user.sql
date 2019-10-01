f8f7fc83a477afccd5b4cb278d0230070719988c
vslobo <antilamer1709@gmail.com>
2019-10-01 17:48:02 +0300

    test user script, for unit tests

---
+++ src/main/resources/db/migration/V0.3__create_test_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO thing_tracker.user (email, full_name, password) VALUES ('testUser@gmail.com', 'testUser', 'testUserPassword');
+INSERT INTO thing_tracker.user_role (user_id, role_id) VALUES (1, 1);
