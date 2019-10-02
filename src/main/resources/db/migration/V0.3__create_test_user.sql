f0531dbeba5d940133ac45f743cd962fd426e7d7
vslobo <antilamer1709@gmail.com>
2019-10-02 17:08:20 +0300

    flyway, add test users

--- src/main/resources/db/migration/V0.3__create_test_user.sql
+++ src/main/resources/db/migration/V0.3__create_test_user.sql
@@ -1,2 +1,12 @@
-INSERT INTO thing_tracker.user (email, full_name, password) VALUES ('testUser@gmail.com', 'testUser', 'testUserPassword');
-INSERT INTO thing_tracker.user_role (user_id, role_id) VALUES (1, 1);
+DO $$
+DECLARE created_id INTEGER;
+
+BEGIN
+INSERT INTO thing_tracker.user (email, full_name, password) VALUES ('testUser@gmail.com', 'testUser', 'testUserPassword') RETURNING id INTO created_id;
+INSERT INTO thing_tracker.user_role (user_id, role_id) VALUES (created_id, 1);
+
+INSERT INTO thing_tracker.user (email, full_name, password) VALUES ('testUser2@gmail.com', 'testUser2', 'testUserPassword2') RETURNING id INTO created_id;
+INSERT INTO thing_tracker.user_role (user_id, role_id) VALUES (created_id, 1);
+INSERT INTO thing_tracker.user_role (user_id, role_id) VALUES (created_id, 2);
+
+END $$
