5a51169c6b226c945c707fd7c0b79c3344230dd2
vslobo <antilamer1709@gmail.com>
2019-12-23 11:32:01 +0200

    refactoring, optimised imports

--- src/main/java/com/antilamer/thingTracker/controller/UserMessageController.java
+++ src/main/java/com/antilamer/thingTracker/controller/UserMessageController.java
@@ -1,17 +1,14 @@
 package com.antilamer.thingTracker.controller;
 
 import com.antilamer.thingTracker.dto.MessageDTO;
-import com.antilamer.thingTracker.dto.ResponseToMessageDTO;
-import com.antilamer.thingTracker.exception.UnauthorizedException;
 import com.antilamer.thingTracker.service.UserMessageBO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
