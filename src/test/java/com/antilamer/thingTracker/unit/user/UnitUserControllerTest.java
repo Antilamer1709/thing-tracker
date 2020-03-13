c5ab4bf9d0e8539de7ff034c6dbb652647f47d16
vslobo <antilamer1709@gmail.com>
2020-03-13 10:35:56 +0200

    UnitUserControllerTest

--- src/test/java/com/antilamer/thingTracker/unit/user/UnitUserControllerTest.java
+++ src/test/java/com/antilamer/thingTracker/unit/user/UnitUserControllerTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
-public class UserControllerTest {
+public class UnitUserControllerTest {
 
     private MockMvc mockMvc;
 
